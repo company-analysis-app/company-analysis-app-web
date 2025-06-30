e5418cd41c11facd4d630e4f0e4b8032cab267b4
kyusangshim <ks030788@naver.com>
2025-06-30 21:32:39 +0900

    Revert "fix: #9 App.tsx에서 라우터 수정"
    This reverts commit 8a2b32024ab0096bf64154003c9352649f13a9ef.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import LoginPage from "./pages/LoginPage"
 import DashboardPage from "./pages/DashboardPage"
 import CompanyDetailPage from "./pages/CompanyDetailPage"
 import ProfilePage from "./pages/ProfilePage"
+import CompanySearchPage from "./pages/CompanySearchPage"
 import NavBar from "./components/Navbar"
 
 // 보호된 라우트 컴포넌트
@@ -45,6 +46,8 @@ const AppContent: React.FC = () => {
 
         {/* 보호된 라우트들 */}
         <Route path="/" element={<ProtectedRoute> <DashboardPage /> </ProtectedRoute>} />
+        <Route path="/dashboard" element={<ProtectedRoute> <DashboardPage /> </ProtectedRoute>} />
+        <Route path="/search" element={<ProtectedRoute> <CompanySearchPage /> </ProtectedRoute>} />
         <Route path="/company/:companyName" element={<ProtectedRoute> <CompanyDetailPage /> </ProtectedRoute>} />
         <Route path="/profile" element={<ProtectedRoute> <ProfilePage /> </ProtectedRoute>} />
 
